a208f496215299ed443aa03a88edd9451165a517
HaoHan <haohan11pfer@protonmail.com>
2022-10-27 03:00:03 +0800

    Add form tag

--- src/components/AppointPage.js
+++ src/components/AppointPage.js
@@ -1,9 +1,28 @@
 
+import Header from "./Header"
+
 export default function AppointPage() {
+
+    const actions = {
+        "CREATE": "create",
+        "SEARCH": "search",
+        "CANCEL": "cancel",
+    }
+
     return (
-        <>
-            <h1>Hello</h1>
-            <div>Hello</div>
-        </>
+        <div className="appoint-page">
+            <Header />
+            <form>
+                <fieldset id={`${actions["SEARCH"]}-appoint`}>
+                    <h1>預約車輛</h1>
+                </fieldset>
+                <fieldset id="search-appoint">
+                    <h1>查詢預約</h1>
+                </fieldset>
+                <fieldset id="cancel-appoint">
+                    <h1>取消預約</h1>
+                </fieldset>
+            </form>
+        </div>
     )
 }

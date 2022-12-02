22c8ddc91f008244f49d22aa5a5bd62941b8c6f9
HaoHan <haohan11pfer@protonmail.com>
2022-12-02 08:35:49 +0800

    Add globalVariable.js

--- src/globalVariables.js
+++
@@ -1,7 +0,0 @@
-export const 
-    REACT_APP_BASE_URL = 'http://localhost:1323',
-    REACT_APP_PATH_LOGIN = 'login',
-    REACT_APP_PATH_USERS = 'users',
-    REACT_APP_PATH_APPOINTMENT = 'booking/appointments',
-    TOKEN_SAVE_NAME = 'car-booking-token',
-    TOKEN_NAME = 'token'

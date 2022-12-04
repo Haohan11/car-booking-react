86b22d01966ea8edd6dcca2db944923128c5e8f8
HaoHan <haohan11pfer@protonmail.com>
2022-12-05 03:01:53 +0800

    rebuild appointment page

--- src/components/AppointPage.js
+++ src/components/AppointPage.js
@@ -1,25 +1,6 @@
-import { useState } from "react"
-import useOneClick from "../hooks/useOneClick"
 import Header from "./Header"
-import Create from "./Create"
-import Search from "./Search"
-import Cancel from "./Cancel"
 
 export default function AppointPage() {
-
-    const actionsGate = {
-        "create": <Create />,
-        "search": <Search />,
-        "cancel": <Cancel />,
-    }
-
-    const [selectRef, selectClicked] = useOneClick()
-
-    const [currentAction, setCurrentAction] = useState("")
-
-    function handleSubmit(event) {
-        event.preventDefault()
-    }
 
     return (
         <div className="appoint-page">
@@ -27,17 +8,9 @@ export default function AppointPage() {
             <div className="user-info">
                 UserName
             </div>
-            <form onSubmit={handleSubmit}>
-                <div className="field actions-select-wrapper">
-                    <select ref={selectRef} onChange={e => setCurrentAction(e.target.value)}>
-                        {selectClicked ? null : <option value="" selected disabled>選擇動作</option>}
-                        <option value="create">預約車輛</option>
-                        <option value="search">查詢預約</option>
-                        <option value="cancel">取消預約</option>
-                    </select>
+            <form>
+                <div>
                 </div>
-                {/* {actionsGate["create"]} */}
-                {currentAction === "" ? null : actionsGate[`${currentAction}`]}
             </form>
         </div>
     )

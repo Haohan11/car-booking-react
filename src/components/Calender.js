6ed0044eb41af72c72869ddbcf091ca0c3d7abf9
HaoHan <haohan11pfer@protonmail.com>
2023-01-02 05:08:58 +0800

    Show calendar dates correctly

--- src/components/Calender.js
+++ src/components/Calender.js
@@ -1,14 +1,36 @@
 export default function Calendar() {
 
-    const tableData = index => <td>
-                        <input name="date" className="date" type="radio"/>
-                        <label>{index}</label>
-                      </td>
-    const tableRow = <tr>{Array.from(Array(7), (element, index) => element = tableData(index))}</tr>
+    const tableData = date =>
+        <td>{ date === 0 ? null : 
+            <>
+                <input name="date" className="date" type="radio"/>
+                <label>{date}</label>
+            </>
+        }</td>
+
+    const table = (() => {
+        const today = new Date()
+        const year = today.getFullYear()
+        const month = today.getMonth()
+        const beginDay = new Date(year, month, 1).getDay()
+        const endDay = new Date(year, month + 1, 0).getDay()
+        const daysNum = new Date(year, month + 1, 0).getDate() + beginDay + (6 - endDay)
+        const dates = Array.from( Array(daysNum), (date, index) => 
+            (index < beginDay || index > daysNum - (7 - endDay)) ? 0 : index - beginDay + 1
+        )
+        
+        const tableRows = Array.from( Array(daysNum / 7), (row, rowIndex) => 
+            <tr>{ 
+                Array.from( Array(7), (element, index) => 
+                    tableData(dates[rowIndex * 7 + index])
+                )
+            }</tr>
+        )
+        
+        return <table>{tableRows}</table>
+    })()
 
     return (
-        <table>
-            {Array(5).fill(tableRow)}
-        </table>
+        table
     )
 }
